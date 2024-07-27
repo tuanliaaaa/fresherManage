d7cac7c044dc4cbbaa532a63f70fdff511a1bc45
nhatt <nhattuan44t@gmail.com>
2024-07-27 10:25:52 +0700

    rename filed potition to position in AccountTable Db

--- src/main/java/com/g11/FresherManage/controller/AccountController.java
+++ src/main/java/com/g11/FresherManage/controller/AccountController.java
@@ -5,6 +5,8 @@ import com.g11.FresherManage.entity.Account;
 import com.g11.FresherManage.service.AccountService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -29,10 +31,39 @@ import java.util.List;
 public class AccountController {
     private final AccountService accountService;
     @Operation( summary = "Get infor of logged-in user",
-            description =  "Retrieve information of the currently logged-in user. This Api is requires token. ",
+            description =  "Retrieve information of the currently logged-in user.  ",
             security = @SecurityRequirement(name = "bearerAuth"))
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\n" +
+                            "  \"status\": 200,\n" +
+                            "  \"message\": \"success\",\n" +
+                            "  \"data\": {\n" +
+                            "    \"iduser\": 1,\n" +
+                            "    \"username\": \"1\",\n" +
+                            "    \"avatar\": \"4\",\n" +
+                            "    \"firstName\": \"2\",\n" +
+                            "    \"lastName\": \"2\",\n" +
+                            "    \"email\": \"2\",\n" +
+                            "    \"phone\": \"2\",\n" +
+                            "    \"position\": null,\n" +
+                            "    \"is_active\": \"2\",\n" +
+                            "    \"curentWorking\": \"3,\",\n" +
+                            "    \"roles\": [\n" +
+                            "      {\n" +
+                            "        \"idRole\": 5,\n" +
+                            "        \"roleName\": \"ROLE_MARKETDIRECTOR\"\n" +
+                            "      },\n" +
+                            "      {\n" +
+                            "        \"idRole\": 3,\n" +
+                            "        \"roleName\": \"ROLE_MENTOR\"\n" +
+                            "      }\n" +
+                            "    ]\n" +
+                            "  },\n" +
+                            "  \"timestamp\": \"2024-07-27\"\n" +
+                            "}"))),
 
     })
     @PreAuthorize("isAuthenticated()")
